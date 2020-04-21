54952b4333265a813d28539835402137d0368ef9
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2020-04-21 23:42:38 +0200

    Improve closed pledge view

--- src/Web/PledgeManager.Web/BsonDocumentExtensions.cs
+++ src/Web/PledgeManager.Web/BsonDocumentExtensions.cs
@@ -28,6 +28,27 @@ namespace PledgeManager.Web {
             };
         }
 
+        public static string ReadSurveyValueToHuman(this BsonDocument d, SurveyElementBase survey) {
+            string defValue = survey switch
+            {
+                SurveyElementCheckbox scb => scb.DefaultToChecked ? "Sì" : "No",
+                _ => string.Empty,
+            };
+
+            if (d == null) {
+                return defValue;
+            }
+            if (!d.TryGetValue(survey.Name, out var v)) {
+                return defValue;
+            }
+
+            return survey switch
+            {
+                SurveyElementCheckbox _ => v.AsBoolean ? "Sì" : "No",
+                _ => v.AsString
+            };
+        }
+
     }
 
 }
