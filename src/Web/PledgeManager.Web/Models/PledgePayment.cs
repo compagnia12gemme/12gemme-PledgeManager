ceb77bb81e5cd939aec874336e75f21409ab4cbc
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2020-04-21 10:25:01 +0200

    Fix pledge payment model

--- src/Web/PledgeManager.Web/Models/PledgePayment.cs
+++ src/Web/PledgeManager.Web/Models/PledgePayment.cs
@@ -1,19 +1,24 @@
-﻿using MongoDB.Bson.Serialization.Attributes;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace PledgeManager.Web.Models {
+
     public class PledgePayment {
 
         [BsonElement("orderId")]
+        [BsonIgnoreIfNull]
         public string OrderId { get; set; }
 
         [BsonElement("payerId")]
+        [BsonIgnoreIfNull]
         public string PayerId { get; set; }
 
         [BsonElement("payerEmail")]
+        [BsonIgnoreIfNull]
         public string PayerEmail { get; set; }
 
         [BsonElement("value")]
@@ -23,5 +28,9 @@ namespace PledgeManager.Web.Models {
         [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
         public DateTime Timestamp { get; set; }
 
+        [BsonExtraElements]
+        public BsonDocument CatchAll { get; set; }
+
     }
+
 }
